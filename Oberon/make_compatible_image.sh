e99a2174fb9de86e71ce274ac4a20e43ba768a35
Michael Schierl <schierlm@gmx.de>
2014-05-05 23:03:21 +0200

    Add RealCalc and PCLink1 to compatible image

--- Oberon/make_compatible_image.sh
+++ Oberon/make_compatible_image.sh
@@ -5,5 +5,11 @@ set -e
 
 patch -d work <detect-screen-size.patch
 cp Clipboard.Mod.Compatible.txt work/Clipboard.Mod.txt
+patch -d work <OberonFromScratch.Compatible.patch
 
 ./derive-files.sh
+
+sed 's/Calc.Tool/Calc.Tool RealCalc.Tool/' \
+    -i work/System.Tool.Full.txt
+sed 's/ResourceMonitor.Run/PCLink1.Run  ResourceMonitor.Run/' \
+    -i work/System.Tool.Full.txt
