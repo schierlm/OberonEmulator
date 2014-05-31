3c370ae25627d918503658a6f524832b306edc72
Michael Schierl <schierlm@gmx.de>
2014-05-31 21:54:46 +0200

    Automate build process
    Add Batch.Mod which can run multiple commands and run GC in between and
    verifies the checksum of the Log output.
    
    Use it to automate the "OberonFromScratch" build.
    
    Also apply all the proposed patches before building.

--- Oberon/make.sh
+++ Oberon/make.sh
@@ -11,4 +11,11 @@ patch -d work <power-management-keyboard-unresponsive.patch
 patch -d work <reduce-filesystem-offset.patch
 patch -d work <reality-lost.patch
 
+patch -d work <../ProposedPatches/initialize-local-variables.patch
+patch -d work <../ProposedPatches/system-clear-init-buf.patch
+patch -d work <../ProposedPatches/log-allocation-failures.patch
+patch -d work <../ProposedPatches/trap-backtrace.patch
+patch -d work <../ProposedPatches/better-display-compatibility.patch
+patch -d work <../ProposedPatches/increment-fix.patch
+
 ./derive-files.sh
--- Oberon/make_compatible_image.sh
+++ Oberon/make_compatible_image.sh
@@ -6,7 +6,14 @@ set -e
 patch -d work <detect-screen-size.patch
 patch -d work <pclink-globbing.patch
 cp Clipboard.Mod.Compatible.txt work/Clipboard.Mod.txt
-patch -d work <OberonFromScratch.Compatible.patch
+cp OberonFromScratch.Tool.Compatible.txt work/OberonFromScratch.Tool.txt
+
+patch -d work <../ProposedPatches/initialize-local-variables.patch
+patch -d work <../ProposedPatches/system-clear-init-buf.patch
+patch -d work <../ProposedPatches/log-allocation-failures.patch
+patch -d work <../ProposedPatches/trap-backtrace.patch
+patch -d work <../ProposedPatches/better-display-compatibility.patch
+patch -d work <../ProposedPatches/increment-fix.patch
 
 ./derive-files.sh
 
