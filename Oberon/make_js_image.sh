f77d2301e55df65ac3720ccaa37a682b2a511204
Michael Schierl <schierlm@gmx.de>
2014-06-07 20:15:12 +0200

    Rename files for building JS image
    In preparation of rebasing all patches so that the Compatible image is the
    normal one and the JS image is based on the Compatible image.
    
    This commit keeps the generated images unchanged.

--- Oberon/make_js_image.sh
+++ Oberon/make_js_image.sh
@@ -11,6 +11,8 @@ patch -d work <power-management-keyboard-unresponsive.patch
 patch -d work <reduce-filesystem-offset.patch
 patch -d work <reality-lost.patch
 
+cp OberonFromScratch.Tool.JavaScript.txt work/OberonFromScratch.Tool.txt
+
 patch -d work <../ProposedPatches/initialize-local-variables.patch
 patch -d work <../ProposedPatches/system-clear-init-buf.patch
 patch -d work <../ProposedPatches/log-allocation-failures.patch
