3f6d47f8323d6d406a28f7fa684d27f5fad93e45
Michael Schierl <schierlm@gmx.de>
2015-06-08 22:59:30 +0200

    Make Batch reliable again
    One of the latest OR compiler changes included hex digits in the compiler
    output, resulting in Batch checksums to change too frequently. Patch the
    compiler to print a backtick before variable parts, and ignore those parts
    (hex digits and spaces) after them.

--- Oberon/apply-proposed-patches.sh
+++ Oberon/apply-proposed-patches.sh
@@ -6,3 +6,4 @@ patch -d work <../ProposedPatches/system-clear-init-buf.patch
 patch -d work <../ProposedPatches/log-allocation-failures.patch
 patch -d work <../ProposedPatches/trap-backtrace.patch
 patch -d work <../ProposedPatches/better-display-compatibility.patch
+patch -d work <../ProposedPatches/mark-changing-compiler-output.patch
