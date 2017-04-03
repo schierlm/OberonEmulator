fc705d5f07920f166eb8909a60e9abe77c1e1c70
Michael Schierl <schierlm@gmx.de>
2017-04-03 18:42:26 +0200

    Simplify building Experimental Oberon
    Provide a Tool with all commands to build a fresh JS Disk Image from the
    working system, as well as a script to patch the source files for doing
    so.

---
+++ Oberon/make_experimental_oberon_tools.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+./get-source.sh
+./derive-files.sh
+patch -d work <fix-js-start-offset.patch
+sed 's/Display/Raster/' -i work/ImageKernel.Mod.txt
+PATCH1='s/Display/Raster/g'
+PATCH2='s/TextFrames, Viewers/Viewers, TextFrames/'
+PATCH3='s/TextFrames.Call(scriptFrame, Texts.Pos(script) - 1, FALSE)/Oberon.Activate(scriptFrame, scriptFrame.text, Texts.Pos(script) - 1)/'
+PATCH4='s/Viewers.This(/Viewers.This(Viewers.CurDisplay, /'
+sed "$PATCH1;$PATCH2;$PATCH3;$PATCH4" -i work/Batch.Mod.txt
+mkdir keep
+mv work/Batch* work/BuildExp* work/Image* keep
+rm work/*
+mv keep/* work
+rmdir keep
