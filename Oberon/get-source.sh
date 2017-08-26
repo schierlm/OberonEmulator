f318b42b9de4d3849e5efc55619bf2f07949769e
Colby Russell <96115c2ddb746683@x.colbyrussell.com>
2017-08-26 08:33:29 -0500

    Make fetch scripts use custom user agent string
    inf.ethz.ch recently began restricting certain user agent strings.  See
    <https://github.com/Spirit-of-Oberon/wirth-personal/commit/12a8e7da74f>

--- Oberon/get-source.sh
+++ Oberon/get-source.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 set -e
+
+USER_AGENT="Mozilla/5.0 oberuntime-build/1.0"
+
 mkdir -p download
 cd download
-wget -nc http://projectoberon.net/zip/inner.zip
-wget -nc http://projectoberon.net/zip/outer.zip
-wget -nc http://projectoberon.net/zip/systools.zip
-wget -nc http://projectoberon.net/zip/net.zip
-wget -nc http://projectoberon.net/zip/or.zip
-wget -nc http://projectoberon.net/zip/graph.zip
-wget -nc http://projectoberon.net/zip/apptools.zip
-wget -nc http://www.inf.ethz.ch/personal/wirth/ProjectOberon/Sources/PCLink1.Mod.txt
+wget -nc -U "$USER_AGENT" http://projectoberon.net/zip/inner.zip
+wget -nc -U "$USER_AGENT" http://projectoberon.net/zip/outer.zip
+wget -nc -U "$USER_AGENT" http://projectoberon.net/zip/systools.zip
+wget -nc -U "$USER_AGENT" http://projectoberon.net/zip/net.zip
+wget -nc -U "$USER_AGENT" http://projectoberon.net/zip/or.zip
+wget -nc -U "$USER_AGENT" http://projectoberon.net/zip/graph.zip
+wget -nc -U "$USER_AGENT" http://projectoberon.net/zip/apptools.zip
+wget -nc -U "$USER_AGENT" http://www.inf.ethz.ch/personal/wirth/ProjectOberon/Sources/PCLink1.Mod.txt
 cd ..
 rm -rf work
 mkdir work
