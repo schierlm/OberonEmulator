82c641cef80c6b086dd52a98454d3b902e68e914
Colby Russell <96115c2ddb746683@x.colbyrussell.com>
2017-10-02 08:55:22 -0500

    Refactor handleEvent to eliminate comment

--- JS/webdriver.js
+++ JS/webdriver.js
@@ -240,14 +240,12 @@ function WebDriver(imageName, width, height) {
 	// DOM Event handling
 
 	$proto.handleEvent = function(event) {
-		// NB: `handleEvent` is specced in the DOM standard to be a void method.
-		// The `return` in each case here is used for control flow only.
 		switch (event.type) {
-			case "load": return this._onLoad(event);
-			case "mousemove": return this._onMouseMove(event);
-			case "mousedown": return this._onMouseButton(event);
-			case "mouseup": return this._onMouseButton(event);
-			case "contextmenu": return event.preventDefault();
+			case "load": return void(this._onLoad(event));
+			case "mousemove": return void(this._onMouseMove(event));
+			case "mousedown": return void(this._onMouseButton(event));
+			case "mouseup": return void(this._onMouseButton(event));
+			case "contextmenu": return void(event.preventDefault());
 			default: throw new Error("got event " + event.type);
 		}
 	};
