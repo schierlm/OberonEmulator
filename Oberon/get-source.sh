eaf76a5a970117168ca4e26719b0522b80570a72
Michael Schierl <schierlm@gmx.de>
2017-11-18 19:33:11 +0100

    Update to latest ProjectOberon source
    Pull the Oberon files from <Spirit-of-Oberon/wirth-personal> repo as a
    submodule, instead of downloading them from the ETHZ website. That way,
    everybody will get the exact same version unless they update the
    submodule themselves, making the build process more reliable.
    
    Update patches and checksums for the new source (only JS images tested).

--- Oberon/get-source.sh
+++ Oberon/get-source.sh
@@ -5,20 +5,14 @@ USER_AGENT="Mozilla/5.0 oberuntime-build/1.0"
 
 mkdir -p download
 cd download
-wget -nc -U "$USER_AGENT" http://projectoberon.net/zip/inner.zip
-wget -nc -U "$USER_AGENT" http://projectoberon.net/zip/outer.zip
 wget -nc -U "$USER_AGENT" http://projectoberon.net/zip/systools.zip
-wget -nc -U "$USER_AGENT" http://projectoberon.net/zip/net.zip
-wget -nc -U "$USER_AGENT" http://projectoberon.net/zip/or.zip
-wget -nc -U "$USER_AGENT" http://projectoberon.net/zip/graph.zip
 wget -nc -U "$USER_AGENT" http://projectoberon.net/zip/apptools.zip
-wget -nc -U "$USER_AGENT" http://www.inf.ethz.ch/personal/wirth/ProjectOberon/Sources/PCLink1.Mod.txt
 cd ..
 rm -rf work
 mkdir work
 cd work
 for i in ../download/*.zip; do unzip $i; done
-cp ../download/*.txt .
+cp ../wirth-personal/personal/wirth/ProjectOberon/Sources/*.txt .
 dos2unix *.txt
 cd ..
 cp *.Mod.txt *.Tool.txt work
