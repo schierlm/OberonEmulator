651cf14689855c737edf35c9d71e69774f211208
Michael Schierl <schierlm@gmx.de>
2018-09-17 20:49:21 +0200

    Keep filesystem internals out of System.Mod
    Instead of manually loading the file header from disk and reading the
    attributes from there, extract this functionality into
    FileDir.GetAttributes function.
    
    Yes, this requires recompiling the inner core, but it makes it easier to
    swap filesystem implementations.

--- Oberon/apply-proposed-patches.sh
+++ Oberon/apply-proposed-patches.sh
@@ -7,3 +7,4 @@ patch -d work <../ProposedPatches/trap-backtrace.patch
 patch -d work <../ProposedPatches/better-display-compatibility.patch
 patch -d work <../ProposedPatches/mark-changing-compiler-output.patch
 patch -d work <../ProposedPatches/memory-allocator-robust-fail.patch
+patch -d work <../ProposedPatches/filesystem-encapsulation.patch
