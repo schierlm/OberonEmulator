schierlm/OberonEmulator

commit c355a8fd7745cad87e93e7630b59fa203e14b72d

Refactor WebDriver#wait, startMillis, waitMillis

JS/webdriver.js
@@ -6,7 +6,9 @@ window.onload = function() {
 
 function WebDriver() {
 	this.keyBuffer = [];
+	this.waitMillis = 0;
 
+	this.startMillis = Date.now();
 	emuInit();
 }
 
@@ -15,6 +17,12 @@ function WebDriver() {
 
 	$proto.cpuTimeout = null;
 	$proto.keyBuffer = null;
+	$proto.startMillis = null;
+	$proto.waitMillis = null;
+
+	$proto.__defineGetter__("tickCount", function() {
+		return Date.now() - this.startMillis;
+	});
 
 	$proto.reset = function(cold) {
 		cpuReset(cold);
@@ -24,5 +32,14 @@ function WebDriver() {
 	$proto.resume = function() {
 		if (this.cpuTimeout != null) window.clearTimeout(this.cpuTimeout);
 		this.cpuTimeout = window.setTimeout(cpuRun, 1);
+	};
+
+	$proto.wait = function(x) {
+		if (this.waitMillis === -1) {
+			this.waitMillis = 0;
+		}
+		else {
+			this.waitMillis = this.startMillis + x;
+		}
 	};
 }

JS/cpu.js
@@ -28,10 +28,10 @@ function cpuResume() {
 function cpuRun() {
 	if (!running) return;
 	var now = Date.now();
-	for(var i=0; i < 200000 && waitMillis < now; i++) {
+	for(var i=0; i < 200000 && emulator.waitMillis < now; i++) {
 		cpuSingleStep();
 	}
-	cpuTimeout = setTimeout(cpuRun, Math.max(waitMillis-Date.now(), 10));
+	cpuTimeout = setTimeout(cpuRun, Math.max(emulator.waitMillis - Date.now(), 10));
 }
 
 function cpuSingleStep() {

JS/memory.js
@@ -31,7 +31,6 @@ function memWriteWord(wordAddress, value) {
 		}
 }
 
-var startMillis = Date.now(), waitMillis = 0;
 var paravirtPtr = 0;
 var mouse = 0;
 var clipboardBuffer = '', clipboardRemaining = 0;
@@ -39,7 +38,7 @@ var clipboardBuffer = '', clipboardRemaining = 0;
 function memReadIOWord(wordAddress) {
 	switch (wordAddress * 4 - IOStart) {
 	case 0: {
-		return (Date.now() - startMillis) | 0;
+		return emulator.tickCount | 0;
 	}
 	case 24: {
 		var _mouse = mouse;
@@ -73,10 +72,7 @@ function memReadIOWord(wordAddress) {
 function memWriteIOWord(wordAddress, value) {
 	switch (wordAddress * 4 - IOStart) {
 	case 0: {
-		if (waitMillis == -1)
-			waitMillis = 0;
-		else
-			waitMillis = startMillis + value;
+		emulator.wait(value);
 		break;
 	}
 	case 4: {
@@ -133,7 +129,7 @@ function hwMouseMoved(mouse_x, mouse_y) {
 		mouse = (mouse & ~0x00FFF000) | (mouse_y << 12);
 	}
 	if (mouse != oldMouse) {
-		waitMillis = -1;
+		emulator.wait(-1);
 		cpuResume();
 	}
 }
@@ -147,13 +143,13 @@ function hwMouseButton(button, down) {
 			mouse &= ~bit;
 		}
 	}
-	waitMillis = -1;
+	emulator.wait(-1);
 	cpuResume();
 }
 
 function hwKeyboardInput(keyChar) {
 	emulator.keyBuffer.push(keyChar << 24);
-	waitMillis = -1;
+	emulator.wait(-1);
 	cpuResume();
 }
 

commit 2ff14d7c5a61b8a9d8d288aec01688ed0b9488f0

Add WebAssembly accelerated version of JS emulator
Also fix a small bug introduced in
c355a8fd7745cad87e93e7630b59fa203e14b72d: Since the two "wait" paths got
merged into one, it could happen that an event gets lost and was
detected only after the WAIT call timed out. In the correct code, when
setting waitMillis to -1, it requires at least two calls to WAIT from
within Oberon without any IO activity between them, before the emulator
would enter the wait state (to ensure that any events that happened
between polling the IO addresses and calling WAIT would not get lost).
As the -1 value got rewritten to `startMillis - 1`, it does no longer
trigger this special handling, so it would start sleeping after the
first WAIT call even if there might have been pending IO activity.

JS/risc-wasm.js
@@ -1,0 +1,138 @@
+function RISCMachine(romWords) {
+	this.megabytes = 1;
+	if ((romWords[255] & 0xFFFFFF) == 0x3D424D) {
+		var mb = (romWords[255] >>> 24) & 0xF;
+		if (mb == 0) mb = 16;
+		this.megabytes = mb;
+	}
+	this.bootROM = romWords;
+}
+
+(function($proto) {
+	var $proto = RISCMachine.prototype;
+
+	$proto.palette = null;
+
+	$proto.memWriteWord = function(address, value) {
+		this.wasm.exports.memWriteWord(address, value);
+	}
+
+	$proto.memReadPalette = function(address0) {
+		if (this.palette == null) {
+			this.palette = [
+				0x000000, 0x000080, 0x008000, 0x008080,	0x800000, 0x800080, 0x808000, 0x808080,
+				0xc0c0c0, 0x0000ff, 0x00ff00, 0x00ffff,	0xff0000, 0xff00ff, 0xffff00, 0xffffff
+			];
+		}
+		return this.palette[address0 / 4 | 0];
+	}
+
+	$proto.memWritePalette = function(address0, val) {
+		if (this.palette != null) {
+			var col = address0 / 4 | 0;
+			this.palette[col] = val & 0xFFFFFF;
+		}
+	}
+
+	$proto.memReadIO = function(address0) {
+		switch (address0) {
+			case  0: return emulator.getTickCount();
+			case  8: return emulator.link.getData();
+			case 12: return emulator.link.getStatus();
+			case 24: return emulator.getInputStatus();
+			case 28: return emulator.getKeyCode();
+			case 40: return emulator.clipboard.getSize();
+			case 44: return emulator.clipboard.getData();
+			default: return 0;
+		}
+	}
+
+	$proto.memWriteIO = function(address0, val) {
+		switch (address0) {
+			case  0: return void(this.wait(val));
+			case  4: return void(emulator.registerLEDs(val));
+			case  8: return void(emulator.link.setData(val));
+			case 36: return void(emulator.storageRequest(val, new Int32Array(this.wasm.exports.memory.buffer, this.wasm.exports.getRAMBase(), this.wasm.exports.getRAMSize()/4)));
+			case 40: return void(emulator.clipboard.expect(val));
+			case 44: return void(emulator.clipboard.putData(val));
+		}
+	}
+
+	$proto.wait = function(x) {
+		if (this.wasm.exports.getWaitMillis() === -1) {
+			this.wasm.exports.setWaitMillis(0);
+		} else {
+			this.wasm.exports.setWaitMillis(emulator.startMillis + x);
+		}
+	}
+
+	$proto.cpuReset = function(cold) {
+		this.wasm.exports.cpuReset(cold);
+	}
+
+	$proto.cpuRun = function() {
+		this.wasm.exports.cpuRun0(Date.now());
+	}
+
+	$proto.cpuSingleStep = function() {
+		this.wasm.exports.cpuSingleStep();
+	}
+
+	$proto.getBootROM = function() {
+		return this.bootROM;
+	}
+
+	$proto.getDisplayStart = function() {
+		return this.wasm.exports.getDisplayStart();
+	}
+
+	$proto.getWaitMillis = function() {
+		return this.wasm.exports.getWaitMillis();
+	}
+
+	$proto.resetWaitMillis = function() {
+		this.wasm.exports.setWaitMillis(-1);
+	}
+
+	RISCMachine.Initialize = function(fetchCallback, finishCallback) {
+		fetchCallback("risc-wasm.wasm", function(arrayBuffer) {
+			WebAssembly.compile(arrayBuffer).then(module => {
+				RISCMachine.wasm = module;
+				finishCallback();
+			});
+		});
+	}
+
+	$proto.Initialize = function(callback) {
+		var that = this;
+		WebAssembly.instantiate(RISCMachine.wasm, {
+		"risc-wasm": {
+			memReadPalette(address) {
+				return that.memReadPalette(address);
+			},
+			memWritePalette(address, val) {
+				return that.memWritePalette(address, val);
+			},
+			memReadIO(address) {
+				return that.memReadIO(address);
+			},
+			memWriteIO(address, val) {
+				return that.memWriteIO(address, val);
+			},
+			registerVideoChange(offset, val) {
+				return emulator.registerVideoChange(offset, val, that.palette);
+			}
+		},
+		"env": {
+			abort(msg, file, line, column) {
+				console.error("abort called at risc-wasm.ts:" + line + ":" + column);
+			}
+		},
+		}).then(instance => {
+			this.wasm = instance;
+			var romBase = this.wasm.exports.Initialize(this.megabytes);
+			new Int32Array(this.wasm.exports.memory.buffer).set(this.bootROM, romBase/4);
+			callback();
+		});
+	}
+})();

JS/risc-wasm.ts
@@ -1,0 +1,296 @@
+// This is AssemblyScript; you can compile it to WebAssembly on https://webassembly.studio/
+
+declare function memReadPalette(address0: i32): i32;
+declare function memWritePalette(address0: i32, val: i32): void;
+declare function memReadIO(address0: i32): i32;
+declare function memWriteIO(address0: i32, val: i32): void;
+declare function registerVideoChange(offset: i32, val: i32): void;
+
+const REGISTER_BASE: usize = HEAP_BASE;
+const ROM_BASE: usize = REGISTER_BASE + 16 * 4;
+const RAM_BASE: usize = ROM_BASE + 1024;
+
+var flag_Z: bool = false;
+var flag_N: bool = false;
+var flag_C: bool = false;
+var flag_V: bool = false;
+var regPC: i32 = 0;
+var regH: i32 = 0;
+
+var ROMStart: i32 = 0x0FE000;
+var DisplayStart: i32 = 0x0E7F00;
+var PaletteStart: i32 = 0x0FFF80;
+var IOStart: i32 = 0x0FFFC0;
+var MemSize: i32 = 0x100000;
+var waitMillis: i32 = 0;
+
+export function Initialize(mb: i32): i32 {
+	if (mb != 1) {
+		MemSize = 0x100000 * mb;
+		var offset: i32 = (mb - 1) * 0x100000;
+		DisplayStart += offset;
+		ROMStart += offset;
+		PaletteStart += offset;
+		IOStart += offset;
+	}
+	memory.grow((RAM_BASE + MemSize + 65535) / 65536 - memory.size());
+	return ROM_BASE;
+}
+
+function cpuGetRegister(id: i32): i32 {
+	return load<i32>(REGISTER_BASE + id * 4);
+}
+
+function cpuPutRegister(id: i32, value: i32): void {
+	store<i32>(REGISTER_BASE + id * 4, value);
+	flag_Z = value == 0;
+	flag_N = value < 0;
+}
+
+export function cpuReset(cold: bool): void {
+	regPC = ROMStart / 4;
+	if (cold) cpuPutRegister(15, 0);
+}
+
+function memReadWord(address: i32, mapROM: bool): i32 {
+	if (mapROM && address >= ROMStart) {
+		return load<i32>(ROM_BASE + (address - ROMStart));
+	} else if (address >= IOStart) {
+		return memReadIO(address - IOStart);
+	} else if (address >= PaletteStart) {
+		DisplayStart = 0x09FF00 + MemSize - 0x100000;
+		return memReadPalette(address - PaletteStart);
+	} else {
+		return load<i32>(RAM_BASE + address);
+	}
+}
+
+export function memWriteWord(address: i32, value: i32): void {
+	if (address >= IOStart) {
+		memWriteIO(address - IOStart, value);
+	} else if (address >= PaletteStart) {
+		memWritePalette(address - PaletteStart, value);
+		var max: i32 = (PaletteStart - DisplayStart) / 4;
+		for (var i: i32 = 0; i < max; i++) {
+			registerVideoChange(i, load<i32>(RAM_BASE + DisplayStart + i * 4));
+		}
+	} else {
+		store<i32>(RAM_BASE + address, value);
+		if (address >= DisplayStart) {
+			registerVideoChange((address - DisplayStart) / 4, value);
+		}
+	}
+}
+
+export function cpuRun0(now: i32): void {
+	for (var i: i32 = 0; i < 200000 && waitMillis < now; ++i) {
+		cpuSingleStep();
+	}
+}
+
+export function cpuSingleStep(): void {
+	const pbit: i32 = 0x80000000;
+	const qbit: i32 = 0x40000000;
+	const ubit: i32 = 0x20000000;
+	const vbit: i32 = 0x10000000;
+
+	var ir: i32 = memReadWord(regPC * 4, true);
+	var a_val: i32, a: i32, b: i32, pos: i32;
+	regPC++;
+
+	if ((ir & pbit) == 0) {
+		a = (ir & 0x0F000000) >> 24;
+		b = (ir & 0x00F00000) >> 20;
+		var op: i32 = (ir & 0x000F0000) >> 16;
+		var im: i32 = ir & 0x0000FFFF;
+		var c: i32 = ir & 0x0000000F;
+
+		var b_val: i32, c_val: i32;
+		b_val = cpuGetRegister(b);
+		if ((ir & qbit) == 0) {
+			c_val = cpuGetRegister(c);
+		} else if ((ir & vbit) == 0) {
+			c_val = im;
+		} else {
+			c_val = 0xFFFF0000 | im;
+		}
+		switch (op) {
+			case 0: {
+				if ((ir & ubit) == 0) {
+					a_val = c_val;
+				} else if ((ir & qbit) != 0) {
+					a_val = c_val << 16;
+				} else if ((ir & vbit) != 0) {
+					a_val = 0xD0 |
+						(flag_N ? 0x80000000 : 0) |
+						(flag_Z ? 0x40000000 : 0) |
+						(flag_C ? 0x20000000 : 0) |
+						(flag_V ? 0x10000000 : 0);
+				} else {
+					a_val = regH;
+				}
+				break;
+			}
+			case 1: {
+				a_val = b_val << (c_val & 31);
+				break;
+			}
+			case 2: {
+				a_val = (b_val) >> (c_val & 31);
+				break;
+			}
+			case 3: {
+				a_val = (b_val >>> (c_val & 31)) | (b_val << (-c_val & 31));
+				break;
+			}
+			case 4: {
+				a_val = b_val & c_val;
+				break;
+			}
+			case 5: {
+				a_val = b_val & ~c_val;
+				break;
+			}
+			case 6: {
+				a_val = b_val | c_val;
+				break;
+			}
+			case 7: {
+				a_val = b_val ^ c_val;
+				break;
+			}
+			case 8: {
+				a_val = (b_val + c_val) | 0;
+				if ((ir & ubit) != 0 && flag_C) {
+					a_val = (a_val + 1) | 0
+				}
+				flag_C = (<u32>a_val) < (<u32>b_val);
+				flag_V = ((~(b_val ^ c_val) & (a_val ^ b_val)) >>> 31) != 0;
+				break;
+			}
+			case 9: {
+				a_val = (b_val - c_val) | 0;
+				if ((ir & ubit) != 0 && flag_C) {
+					a_val = (a_val - 1) | 0;
+				}
+				flag_C = (<u32>a_val) > (<u32>b_val);
+				flag_V = (((b_val ^ c_val) & (a_val ^ b_val)) >>> 31) != 0;
+				break;
+			}
+			case 10: {
+				var tmp: i64;
+				if ((ir & ubit) == 0) {
+					tmp = b_val * c_val;
+				} else {
+					tmp = <i64>((<u32>b_val) * (<u32>c_val));
+				}
+				a_val = <i32>tmp;
+				regH = <i32>((<u64>tmp) >>> 32);
+				break;
+			}
+			case 11: {
+				if ((ir & ubit) == 0) {
+					var h_val: i32 = (b_val % c_val);
+					a_val = b_val / c_val;
+					if (h_val < 0) {
+						h_val += c_val;
+						a_val--;
+					}
+					regH = h_val;
+				} else {
+					a_val = (<u32>b_val) / (<u32>c_val);
+					regH = (<u32>b_val) % (<u32>c_val);
+				}
+				break;
+			}
+			case 13:
+				c_val ^= 0x80000000;
+			// fall through
+			case 12:
+				if ((ir & ubit) == 0 && (ir & vbit) == 0)
+					a_val = reinterpret<i32>(reinterpret<f32>(b_val) + reinterpret<f32>(c_val));
+				if ((ir & ubit) != 0 && (ir & vbit) == 0 && c_val == 0x4B000000)
+					a_val = reinterpret<i32>(<f32>b_val);
+				if ((ir & ubit) == 0 && (ir & vbit) != 0 && c_val == 0x4B000000)
+					a_val = <i32>reinterpret<f32>(b_val);
+				break;
+			case 14:
+				a_val = reinterpret<i32>(reinterpret<f32>(b_val) * reinterpret<f32>(c_val));
+				break;
+			case 15:
+				a_val = reinterpret<i32>(reinterpret<f32>(b_val) / reinterpret<f32>(c_val));
+				break;
+		}
+		cpuPutRegister(a, a_val);
+	} else if ((ir & qbit) == 0) {
+		a = (ir & 0x0F000000) >> 24;
+		b = (ir & 0x00F00000) >> 20;
+		var off: i32 = (ir & 0x000FFFFF) << 12 >> 12;
+
+		var address: i32 = <i32>(((<u32>cpuGetRegister(b)) + (<u32>off)) % MemSize);
+		if ((ir & ubit) == 0) {
+			if ((ir & vbit) == 0) {
+				a_val = memReadWord(address, false);
+			} else {
+				a_val = (memReadWord(address & ~3, false) >>> ((address % 4) * 8)) & 0xff;
+			}
+			cpuPutRegister(a, a_val);
+		} else {
+			if ((ir & vbit) == 0) {
+				memWriteWord(address, cpuGetRegister(a));
+			} else if (address < IOStart) {
+				var w: i32 = memReadWord(address & ~3, false);
+				var shift: i32 = (address & 3) * 8;
+				w &= ~(0xFF << shift);
+				w |= (cpuGetRegister(a) & 0xFF) << shift;
+				memWriteWord(address & ~3, w);
+			} else {
+				memWriteWord(address & ~3, cpuGetRegister(a) & 0xFF);
+			}
+		}
+	} else {
+		var t: bool;
+		switch ((ir >>> 24) & 7) {
+			case 0: t = flag_N; break;
+			case 1: t = flag_Z; break;
+			case 2: t = flag_C; break;
+			case 3: t = flag_V; break;
+			case 4: t = flag_C || flag_Z; break;
+			case 5: t = flag_N != flag_V; break;
+			case 6: t = flag_N != flag_V || flag_Z; break;
+			case 7: t = true; break;
+		}
+		if (t != (((ir >>> 24) & 8) != 0)) {
+			if ((ir & vbit) != 0) {
+				cpuPutRegister(15, regPC * 4);
+			}
+			if ((ir & ubit) == 0) {
+				pos = (cpuGetRegister(ir & 0x0000000F) >>> 0) / 4;
+				regPC = pos % (MemSize / 4);
+			} else {
+				pos = regPC + (ir & 0x00FFFFFF);
+				regPC = pos % (MemSize / 4);
+			}
+		}
+	}
+}
+
+export function getRAMBase(): i32 {
+	return RAM_BASE;
+}
+
+export function getRAMSize(): i32 {
+	return MemSize;
+}
+
+export function setWaitMillis(val: i32): void {
+	waitMillis = val;
+}
+
+export function getDisplayStart(): i32 {
+	return DisplayStart;
+}
+
+export function getWaitMillis(): i32 {
+	return waitMillis;
+}

JS/risc.js
@@ -1,4 +1,4 @@
-function RISCMachine(romWords) {
+function RISCMachine(romWords, callback) {
 	this.registers = new Int32Array(
 		this.GeneralRegisterCount + this.SpecialRegisterCount
 	);
@@ -34,6 +34,7 @@ function RISCMachine(romWords) {
 	RISCMachine.MemSize = $proto.MemSize = 0x100000;
 	RISCMachine.MemWords = $proto.MemWords = (RISCMachine.MemSize / 4);
 	$proto.palette = null;
+	$proto.waitMillis = 0;
 
 	$proto.cpuRegisterSlot = function(id) {
 		if (id < 0 && -id <= this.SpecialRegisterCount) {
@@ -118,12 +119,20 @@ function RISCMachine(romWords) {
 
 	$proto.memWriteIO = function(address, val) {
 		switch (address - this.IOStart) {
-			case  0: return void(emulator.wait(val));
+			case  0: return void(this.wait(val));
 			case  4: return void(emulator.registerLEDs(val));
 			case  8: return void(emulator.link.setData(val));
 			case 36: return void(emulator.storageRequest(val, this.mainMemory));
 			case 40: return void(emulator.clipboard.expect(val));
 			case 44: return void(emulator.clipboard.putData(val));
+		}
+	}
+
+	$proto.wait = function(x) {
+		if (this.waitMillis === -1) {
+			this.waitMillis = 0;
+		} else {
+			this.waitMillis = emulator.startMillis + x;
 		}
 	}
 
@@ -136,6 +145,13 @@ function RISCMachine(romWords) {
 	$proto.cpuReset = function(cold) {
 		this.cpuPutRegister(this.PCID, this.ROMStart / 4);
 		if (cold) this.cpuPutRegister(15, 0);
+	}
+
+	$proto.cpuRun = function() {
+		var now = Date.now();
+		for (var i = 0; i < 200000 && this.waitMillis < now; ++i) {
+			this.cpuSingleStep();
+		}
 	}
 
 	$proto.cpuSingleStep = function() {
@@ -350,6 +366,30 @@ function RISCMachine(romWords) {
 		} else {
 			this.memWriteWord(address | 0, value & 0xFF);
 		}
+	}
+
+	$proto.getBootROM = function() {
+		return this.bootROM;
+	}
+
+	$proto.getDisplayStart = function() {
+		return this.DisplayStart;
+	}
+
+	$proto.getWaitMillis = function() {
+		return this.waitMillis;
+	}
+
+	$proto.resetWaitMillis = function() {
+		this.waitMillis = -1;
+	}
+
+	RISCMachine.Initialize = function(fetchCallback, finishCallback) {
+		finishCallback();
+	}
+
+	$proto.Initialize = function(callback) {
+		callback();
 	}
 })();
 

JS/webdriver.js
@@ -17,7 +17,17 @@ window.onload = function() {
 			params.height = h < 576 ? 512 : h < 768 ? 576 : 768;
 		}
 	}
-	emulator = new WebDriver(params.image, params.width, params.height);
+	RISCMachine.Initialize(function(uri, callback) {
+		var request = new XMLHttpRequest();
+		request.addEventListener("load", function(event) {
+			callback(event.target.response);
+		});
+		request.open("GET", uri);
+		request.responseType = "arraybuffer";
+		request.send(null);
+	}, function() {
+		emulator = new WebDriver(params.image, params.width, params.height);
+	});
 }
 
 function WebDriver(imageName, width, height) {
@@ -69,7 +79,6 @@ function WebDriver(imageName, width, height) {
 	$proto.screenUpdater = null;
 	$proto.startMillis = null;
 	$proto.transferHistory = null;
-	$proto.waitMillis = 0;
 	$proto.width = 0;
 	$proto.autosave = false;
 
@@ -146,7 +155,7 @@ function WebDriver(imageName, width, height) {
 					var reader = new ROMFileReader("boot.rom", contents, name);
 					return void(reader.prepareContentsThenNotify(this));
 				} else {
-					rom = this.machine.bootROM;
+					rom = this.machine.getBootROM();
 				}
 			} else if (this._hasDirMark(contents, 1)) {
 				rom = contents.shift();
@@ -170,8 +179,11 @@ function WebDriver(imageName, width, height) {
 
 		this.disk = disk;
 		this.startMillis = Date.now();
+		var that = this;
 		this.machine = new RISCMachine(rom);
-		this.reset(true);
+		this.machine.Initialize(function() {
+			that.reset(true);
+		});
 	};
 
 	$proto.isDisplayReady = function() {
@@ -194,7 +206,7 @@ function WebDriver(imageName, width, height) {
 	$proto.reset = function(cold) {
 		this.machine.cpuReset(cold);
 		if (cold) {
-			var base = this.machine.DisplayStart;
+			var base = this.machine.getDisplayStart();
 			this.machine.memWriteWord(base, 0x53697A65); // magic value 'Size'
 			this.machine.memWriteWord(base + 4, this.screen.width);
 			this.machine.memWriteWord(base + 8, this.screen.height);
@@ -209,22 +221,10 @@ function WebDriver(imageName, width, height) {
 
 	$proto.run = function() {
 		if (this.paused) return;
-		var now = Date.now();
-		for (var i = 0; i < 200000 && this.waitMillis < now; ++i) {
-			this.machine.cpuSingleStep();
-		}
+		this.machine.cpuRun();
 		this.cpuTimeout = window.setTimeout(
-			this.$run, Math.max(this.waitMillis - Date.now(), 10), this
+			this.$run, Math.max(this.machine.getWaitMillis() - Date.now(), 10), this
 		);
-	};
-
-	$proto.wait = function(x) {
-		if (this.waitMillis === -1) {
-			this.waitMillis = 0;
-		}
-		else {
-			this.waitMillis = this.startMillis + x;
-		}
 	};
 
 	$proto.getTickCount = function() {
@@ -273,7 +273,7 @@ function WebDriver(imageName, width, height) {
 		if (before === after) return;
 
 		this.mouse = after;
-		this.wait(-1);
+		this.machine.resetWaitMillis();
 		this.reschedule();
 	};
 
@@ -287,7 +287,7 @@ function WebDriver(imageName, width, height) {
 				this.mouse &= ~bit;
 			}
 		}
-		this.wait(-1);
+		this.machine.resetWaitMillis();
 		this.reschedule();
 	};
 
@@ -331,7 +331,7 @@ function WebDriver(imageName, width, height) {
 
 	$proto.registerKey = function(keyCode) {
 		this.keyBuffer.push((keyCode << 24) | (keyCode >>> 8 << 16));
-		this.wait(-1);
+		this.machine.resetWaitMillis();
 		this.reschedule();
 	};
 
@@ -372,7 +372,7 @@ function WebDriver(imageName, width, height) {
 		this.ui.autosaveToggle.classList.toggle("checked");
 		if (this.autosave) {
 			window.localStorage.setItem("AUTOSAVE", this.disk.length);
-			window.localStorage.setItem("AUTOSAVE-ROM", btoa(String.fromCharCode.apply(null, new Uint8Array(this.machine.bootROM.buffer))));
+			window.localStorage.setItem("AUTOSAVE-ROM", btoa(String.fromCharCode.apply(null, new Uint8Array(this.machine.getBootROM().buffer))));
 			for(var i = 0; i < this.disk.length; i++) {
 				window.localStorage.setItem("AUTOSAVE-"+i, btoa(String.fromCharCode.apply(null, new Uint8Array(this.disk[i].buffer))));
 			}
@@ -382,7 +382,7 @@ function WebDriver(imageName, width, height) {
 	}
 
 	$proto.dumpROM = function() {
-		this.save("boot.rom", [ this.machine.bootROM ]);
+		this.save("boot.rom", [ this.machine.getBootROM() ]);
 	};
 
 	$proto.save = function(name, content) {
@@ -711,7 +711,7 @@ function ControlBarUI(emulator) {
 	$proto.exportCustomImage = function() {
 		this.exportOptions.romtype[0].checked=true;
 		this.exportOptions.romfile.value="";
-		var mb = (emulator.machine.bootROM[255] & 0xFFFFFF) == 0x3D424D ? (emulator.machine.bootROM[255] >>> 24) & 0xF : 1;
+		var mb = (emulator.machine.getBootROM()[255] & 0xFFFFFF) == 0x3D424D ? (emulator.machine.getBootROM()[255] >>> 24) & 0xF : 1;
 		this.exportOptions.ramsize.value=mb;
 		this.exportOptions.classList.add("open");
 		this.togglePopup(this.systemButton);
@@ -721,7 +721,7 @@ function ControlBarUI(emulator) {
 		var reader = new FileReader();
 		var that = this;
 		if (document.getElementById("romtypecurrent").checked) {
-			return this.doExportPNGWithROM(emulator.machine.bootROM);
+			return this.doExportPNGWithROM(emulator.machine.getBootROM());
 		} else if (document.getElementById("romtypefile").checked) {
 			reader.onload = function() {
 				that.doExportPNGWithROM(DiskFileReader.getContents(reader.result)[0]);
@@ -1008,7 +1008,7 @@ function SerialFIFO(notifyWindow) {
 		this.writePtr = (this.writePtr + 1) % 4096;
 		var thatNotifyWindow = this.notifyWindow;
 		thatNotifyWindow.setTimeout(function() {
-			thatNotifyWindow.emulator.wait(-1);
+			thatNotifyWindow.emulator.machine.resetWaitMillis();
 			thatNotifyWindow.emulator.reschedule();
 		}, 1);
 	};